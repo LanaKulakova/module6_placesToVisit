6e6bd8710363597f3ef3468171ffcd50f973dd6b
LanaKulakova <lanakulakova@gmail.com>
2023-08-23 22:19:51 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import {useState} from "react";
+import {data} from "./data";
 import './App.css';
 
 function App() {
+const [showText, setShowText] = useState(false);
+
+const [places, setPlaces] = useState(data);
+//const [showMore, setShowmore] = useState(false);
+
+
+const removePlace = (id) => {
+  let newPlace = places.filter(place => place.id !== id);
+  setPlaces(newPlace)
+}
+
+const showTextClick = (item) => {
+  item.showMore = !item.showMore
+  setShowText(!showText)
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+<div className="container">
+  <h1>List of {places.length} places to visit in North of France</h1>
+</div>
+
+{places.map((item => {
+  const {id, name, image, description, showMore} = item;
+
+  return (
+    <div key={id}>
+<div className="container"> 
+<h2>{id} - {name}</h2>
+</div>
+
+<div className="container"> 
+<img src={image} width="300px" alt="place" />
+</div>
+
+<div className="container"> 
+<p>{showMore ? description : description.substring(0, 80) + "..."} <button onClick = { () => showTextClick(item)}> {showMore ? "Show less" : "Show more"}</button></p>
+</div>
+
+<div className="container btn">
+<button onClick = {() => removePlace(id) }>Remove item</button>
+</div>
+
+
+    </div>
+  )
+}))}
+
+<div className="btn container ">
+<button onClick = { () => setPlaces([])}>Remove all</button>
+</div>
+
+
+
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,33 @@
+import etretat from "./etretat.jpg";
+import stmichel from "./stmichel.jpg";
+import cancale from "./cancale.jpg";
+
+
+export const data = [
+
+{
+    id: 1,
+    name:"Étretat",
+    image: etretat,
+    description: "Étretat is a town on the north coast of France. It’s known for the striking rock formations carved out of its white cliffs, including the Porte d'Aval arch and L'Aiguille (the Needle), a pillar rising up from the sea. To the north, the cliffside Chapelle Notre-Dame de la Garde has dramatic views. Le Vieux Marché is a market hall in town. Nearby, Le Clos Lupin villa was once home to French author Maurice Leblanc.",
+    showMore: false
+},
+
+{
+    id: 2,
+    name:"Le Mont Saint Michel",
+    image: stmichel,
+    description: "Mont-Saint-Michel is a tidal island and mainland commune in Normandy, France. The island lies approximately one kilometre off France's north-western coast, at the mouth of the Couesnon River near Avranches and is 7 hectares in area.",
+    showMore: false
+},
+
+{
+    id: 3,
+    name:"Cancale",
+    image: cancale,
+    description: "Cancale is located at the western end of the bay of Mont-Saint-Michel, on the coasts of Ille-et-Vilaine (Côte d'Émeraude), fifteen kilometers east of Saint-Malo. The bay of Cancale is delimited by the pointe des Roches Noires in the south and the pointe des Crolles in the north. It is known as the birthplace of Saint Jeanne Jugan.",
+    showMore: false
+},
+
+
+]
